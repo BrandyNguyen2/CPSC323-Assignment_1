685638955d00552fc5514d6b14c78752af7c35fb
bnguyen700 <brandy.nguyen02@gmail.com>
2024-09-19 16:32:35 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Programmers: Salvador Delgado, Brandy Nguyen, Landon Patam, & Nicholas Reeves
+
+keyword = ["integer", "if", "else", "fi", "while", "for", "put", "return"]
+identifier = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", 
+              "p", "q", "r", "s", "t", "u", "v", "w", "x", "y"]
+operator = ["+", "-", "*", "/", "=", "==", "<", ">", "=>", "=<", "and", "or", "not"]
